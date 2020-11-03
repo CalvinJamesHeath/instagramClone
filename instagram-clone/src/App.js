5deb6ea2a24f7bb3805fbb44d3953a75b4376a1a
CalvinJamesHeath <CalvinJamesHeath14@hotmail.com>
2020-11-03 14:59:14 -0300

    Initial commit

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Post from "./Post";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      <Post />
+    </div>
+  );
+}
+
+export default App;
