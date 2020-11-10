4997e21d73584cf09f467971bbc1934574ede997
CalvinJamesHeath <CalvinJamesHeath14@hotmail.com>
2020-11-10 16:58:12 -0300

    Update Post.js

--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -39,11 +39,7 @@ function Post({ user, postId, username, caption, imageURL }) {
   return (
     <div className="post">
       <div className="post__header">
-        <Avatar
-          className="post_avatar"
-          alt="Calvin"
-          src="https://avatars1.githubusercontent.com/u/72168052?s=460&u=195f5b794f0672b65a541b365bb1a296cb215e7b&v=4"
-        />
+        <Avatar className="post_avatar" alt="Calvin" src={imageURL} />
         <h3>{username}</h3>
       </div>
       <img className="post__image" src={imageURL} />
