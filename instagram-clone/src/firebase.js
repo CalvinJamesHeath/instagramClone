542d262ea3e855cce02b3f6f17098686de8aa441
Calvin James Heath <72168052+CalvinJamesHeath@users.noreply.github.com>
2021-03-02 16:26:28 -0300

    Update firebase.js

--- instagram-clone/src/firebase.js
+++ instagram-clone/src/firebase.js
@@ -2,14 +2,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBUo6fdRt9ZaShwZOxJA4RmU0SLf_Whego",
+    apiKey: "",
     authDomain: "instagram-clone-a246d.firebaseapp.com",
     databaseURL: "https://instagram-clone-a246d.firebaseio.com",
     projectId: "instagram-clone-a246d",
     storageBucket: "instagram-clone-a246d.appspot.com",
     messagingSenderId: "903947255757",
-    appId: "1:903947255757:web:b9fd7644349cfd49f78e84",
-    measurementId: "G-60G6QWFZ5J"
+    appId: "",
+    measurementId: ""
 })
 
 
